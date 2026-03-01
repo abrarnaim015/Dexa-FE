d9a77db66e38cc50868f5c7c5db01bb48e1390d4
abrarnaim015 <aabrarnaim24@gmail.com>
2026-03-01 07:11:28 +0700

    add env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:3000", // BE URL
+  baseURL: API_URL, // BE URL
 });
 
 api.interceptors.request.use((config) => {
